d52b0109021b759a4b651ed101d75293971c6e88
foobardrum <tobias@eggut.ch>
2019-03-04 23:11:11 +0100

    added birthday date and registration dateTime

--- src/components/shared/models/User.js
+++ src/components/shared/models/User.js
@@ -1,3 +1,4 @@
+import dateTime from "../../../helpers/dateTime";
 /**
  * User model
  */
@@ -6,13 +7,23 @@ class User {
     this.id = null;
     this.name = null;
     this.username = null;
-    this.birthday = null;
+    this.birthdayDate = null;
     this.token = null;
     this.status = null;
     this.registrationDateTime = null;
     this.games = null;
     this.moves = null;
     Object.assign(this, data);
+    this.registrationDateTime = dateTime.beautifyDateTime(data.registrationDateTime);
+    this.birthdayDate = dateTime.beautifyDate(data.birthdayDate);
+
+  }
+
+  getFormattedUser() {
+      let formattedUser = new User(this);
+      formattedUser.registrationDateTime = dateTime.formatDateTime(formattedUser.registrationDateTime);
+      formattedUser.birthdayDate = dateTime.formatDate(formattedUser.birthdayDate);
+      return formattedUser;
   }
 }
 export default User;
